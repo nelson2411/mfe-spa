ab7125fa4d41273a5bcb8cfde3caa4b1e8539a1b
Nelson Rosales <nelson.rosales83@gmail.com>
2024-05-24 12:37:25 +0200

    chore: Add container deployment workflow and update webpack configuration

---
+++ container/config/webpack.prod.js
@@ -0,0 +1,26 @@
+const { merge } = require("webpack-merge")
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin")
+const commonConfig = require("./webpack.common")
+const packageJson = require("../package.json")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+const domain = process.env.PRODUCTION_DOMAIN
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js", // contenthash is used to cache the file
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container", // This is not strictly necessary, but it's a good practice to set it
+      remotes: {
+        // The key is the name of the remote, the value is the name of the remote and the path to the remoteEntry file
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+}
+
+module.exports = merge(commonConfig, prodConfig)
