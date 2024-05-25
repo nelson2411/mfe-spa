dd215055f78405221f76c213f1d552a44d929492
Nelson Rosales <nelson.rosales83@gmail.com>
2024-05-25 12:01:03 +0200

    chore: Update App.js to use StylesProvider and createGenerateClassname for consistent styling across components

--- container/src/App.js
+++ container/src/App.js
@@ -1,15 +1,25 @@
 import React from "react"
+import {
+  StylesProvider,
+  createGenerateClassname,
+} from "@material-ui/core/styles"
 import { BrowserRouter } from "react-router-dom"
 import MarketingApp from "./components/MarketingApp"
 import Header from "./components/Header"
 
+const generateClassName = createGenerateClassname({
+  productionPrefix: "co",
+})
+
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   )
 }
